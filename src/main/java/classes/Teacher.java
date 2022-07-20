3787d699248d78f654648b4d5b026a2320c84086
FumaTosora <asoukal@t-online.de>
2022-07-20 19:35:09 +0200

    a

--- src/main/java/classes/Teacher.java
+++ src/main/java/classes/Teacher.java
@@ -4,7 +4,24 @@ import java.sql.SQLException;
 
 public class Teacher extends User {
 
+    public void onlogin(User user) throws SQLException {
+        String sql = "SELECT * FROM sqlGrades AS g INNER JOIN sqlStudent AS s ON g.ID = s.ID INNER JOIN sqlTeacher AS t ON s.BEZKL = t.BEZKL INNER JOIN sqlUser AS u ON u.ID = s.ID";
+        ResultSet rs = DBHelper.executeSqlSelectStatement(sql);
 
+        //TODO Ausgabe ändern
+        //BEZKL könnte falsch sein
+
+        while (rs.next()) {
+            if (Arrays.asList(subjects).contains(rs.getString("SUBJECT")) && Arrays.asList(classes).contains(rs.getString("BEZKL"))) {
+               System.out.println(rs.getString("FIRSTNAME") + "   " + 
+                               rs.getString("NAME") + "   " +
+                               rs.getString("SUBJECT") + "   " +
+                               rs.getInt("GRADE_Val")+ "   " +
+                               rs.getString("GRADE_BEZ")); 
+            }
+            
+        }
+    }
 
 
 
