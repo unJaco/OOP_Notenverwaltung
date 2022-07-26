3a22cbaab941c72ec3ed59e6b79f3b2fc1da0c03
unJaco <73906102+unJaco@users.noreply.github.com>
2022-07-26 09:25:02 +0200

    added schoolclass functionality

--- src/main/java/classes/SchoolClass.java
+++ src/main/java/classes/SchoolClass.java
@@ -1,20 +1,44 @@
 package classes;
 
+
+import java.util.ArrayList;
 import java.util.Map;
 
 public class SchoolClass {
 
-    String bezeichnung;
+    private String class_id;
 
-    Student[] students;
-
-    Map<Subject, Teacher> subjects;
+    Map<Integer, Student> studentMap;
 
 
+    public SchoolClass(String class_id, Map<Integer, Student> studentMap) {
+        this.class_id = class_id;
+        this.studentMap = studentMap;
+    }
 
+    public String getClass_id() {
+        return class_id;
+    }
 
+    public void setClass_id(String class_id) {
+        this.class_id = class_id;
+    }
 
+    public Map<Integer, Student> getStudents() {
+        return studentMap;
+    }
 
+    public void setStudents(Map<Integer, Student> studentMap) {
+        this.studentMap = studentMap;
+    }
+
+    public void addStudent(Student student){
+        studentMap.put(student.getId(), student);
+    }
+
+    public Student getStudent(int uid){
+        return studentMap.get(uid);
+    }
 
 
 }
